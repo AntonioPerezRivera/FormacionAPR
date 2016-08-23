3eff9c63398eb0b5db04828e69ff3e3215b750a9
Antonio Miguel Pérez Rivera <antoniom.p.rivera@gmail.com>
2016-08-23 20:12:11 +0200

    Añadido DTO de Zone (Zona de la sala)

---
+++ src/main/java/com/at/library/dto/ZoneDTO.java
@@ -0,0 +1,50 @@
+package com.at.library.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ZoneDTO implements Serializable {
+
+	private static final long serialVersionUID = -2318152166576404732L;
+
+	private Integer id;
+
+	private String name;
+
+	RoomDTO room;
+	
+	List<BookDTO> books;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public RoomDTO getRoom(){
+		return room;
+	}
+	
+	public void setRoom(RoomDTO room){
+		this.room = room;
+	}
+	
+	public List<BookDTO> getBooks(){
+		return books;
+	}
+	
+	public void setBooks(List<BookDTO> books){
+		this.books = books;
+	}
+
+}
