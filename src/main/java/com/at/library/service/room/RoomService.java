74b717c4073fd3383348eaa5c8992fb395e1d372
Antonio Miguel Pérez Rivera <antoniom.p.rivera@gmail.com>
2016-08-23 20:31:16 +0200

    Añadida interfaz inicial RoomService

---
+++ src/main/java/com/at/library/service/room/RoomService.java
@@ -0,0 +1,33 @@
+package com.at.library.service.room;
+
+import java.util.List;
+
+import com.at.library.dto.RoomDTO;
+import com.at.library.model.Room;
+
+public interface RoomService {
+
+	/**
+	 * Realiza la busqueda de todas las salas existentes
+	 * 
+	 * @return listado de salas
+	 */
+	List<RoomDTO> findAll();
+
+	/**
+	 * Transforma una sala en un salaDTO
+	 * 
+	 * @param room
+	 * @return
+	 */
+	RoomDTO transform(Room room);
+
+	/**
+	 * Transforma un salaDTO en una sala
+	 * 
+	 * @param room
+	 * @return
+	 */
+	Room transform(RoomDTO room);
+
+}
