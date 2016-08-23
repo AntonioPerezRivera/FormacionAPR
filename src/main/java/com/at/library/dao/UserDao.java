14ecedc7198ca8905c7e207f76bc071c2a99d2bd
Antonio Miguel Pérez Rivera <antoniom.p.rivera@gmail.com>
2016-08-23 20:05:52 +0200

    Añadida implementacion del servicio de User

--- src/main/java/com/at/library/dao/UserDao.java
+++ src/main/java/com/at/library/dao/UserDao.java
@@ -3,9 +3,9 @@ package com.at.library.dao;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.at.library.model.Book;
+import com.at.library.model.User;
 
 @Repository
-public interface UserDao extends CrudRepository<Book, Integer> {
+public interface UserDao extends CrudRepository<User, Integer> {
 
 }
