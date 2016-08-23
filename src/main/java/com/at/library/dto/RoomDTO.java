11b7eb9b3ea2e24d50e41bad2b9ee3caeed9bd8a
Antonio Miguel Pérez Rivera <antoniom.p.rivera@gmail.com>
2016-08-23 20:27:22 +0200

    Completada implementacion inicial de RoomDTO

--- src/main/java/com/at/library/dto/RoomDTO.java
+++ src/main/java/com/at/library/dto/RoomDTO.java
@@ -1,5 +1,50 @@
 package com.at.library.dto;
 
-public class RoomDTO {
+import java.io.Serializable;
+import java.util.List;
+
+public class RoomDTO implements Serializable {
+	
+	private static final long serialVersionUID = -8678839705725183562L;
+
+	private Integer id;
+
+	private String name;
+
+	private String address;
+	
+	List<ZoneDTO> zones;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getAddress(){
+		return address;
+	}
+	
+	public void setAddress(String address){
+		this.address = address;
+	}
+	
+	public List<ZoneDTO> getZones(){
+		return zones;
+	}
+	
+	public void setZones(List<ZoneDTO> zones){
+		this.zones = zones;
+	}
 
 }
