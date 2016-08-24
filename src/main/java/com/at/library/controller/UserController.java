b025a3806676f292dc18fef54878d8d09abc97e6
Antonio Miguel Pérez Rivera <antoniom.p.rivera@gmail.com>
2016-08-24 19:38:19 +0200

    Implementadas operaciones CRUD basicas en el controlador de User

--- src/main/java/com/at/library/controller/UserController.java
+++ src/main/java/com/at/library/controller/UserController.java
@@ -2,7 +2,10 @@ package com.at.library.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +14,42 @@ import com.at.library.dto.UserDTO;
 import com.at.library.service.user.UserService;
 
 @RestController
+@RequestMapping(value = "/user")
 public class UserController {
 
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value = "/user", method = { RequestMethod.GET })
+	private static final Logger log = org.slf4j.LoggerFactory.getLogger(UserController.class);
+	
+	@RequestMapping(method={RequestMethod.GET})
 	public List<UserDTO> getAll() {
+		log.debug("Buscando todos los usuarios en el sistema");
 		return userService.findAll();
+	}
+	
+	@RequestMapping(method={RequestMethod.POST})
+	public UserDTO create(@RequestBody UserDTO user){
+		log.debug(String.format("Vamos a crear el usuario %s", user));
+		return userService.create(user);
+	}
+	
+	@RequestMapping(value="/{id}", method={RequestMethod.GET})
+	public UserDTO get(@PathVariable("id") Integer id){
+		log.debug(String.format("Recuperando usuario con id: %s",id));
+		return userService.getById(id);
+	}
+
+	@RequestMapping(value="/{id}", method={RequestMethod.PUT})
+	public void update(@PathVariable("id") Integer id, @RequestBody UserDTO user){
+		log.debug(String.format("Vamos a modificar el usuario %s", user));
+		userService.update(user);
+	}
+	
+	@RequestMapping(value="/{id}", method={RequestMethod.PUT})
+	public void delete(@PathVariable("id") Integer id){
+		log.debug(String.format("Vamos a modificar el usuario con id %s", id));
+		userService.delete(id);
 	}
 
 }
