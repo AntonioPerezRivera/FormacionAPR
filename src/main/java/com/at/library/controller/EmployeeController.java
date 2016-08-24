43dc04397a955bbc053ac0919fa6870fbb93ea8b
Antonio Miguel Pérez Rivera <antoniom.p.rivera@gmail.com>
2016-08-24 19:16:12 +0200

    Implementadas operaciones CRUD basicas en el controlador de Employee

--- src/main/java/com/at/library/controller/EmployeeController.java
+++ src/main/java/com/at/library/controller/EmployeeController.java
@@ -2,7 +2,10 @@ package com.at.library.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +14,42 @@ import com.at.library.dto.EmployeeDTO;
 import com.at.library.service.employee.EmployeeService;
 
 @RestController
+@RequestMapping(value = "/employee")
 public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
 
-	@RequestMapping(value = "/employee", method = { RequestMethod.GET })
+	private static final Logger log = org.slf4j.LoggerFactory.getLogger(EmployeeController.class);
+	
+	@RequestMapping(method={RequestMethod.GET})
 	public List<EmployeeDTO> getAll() {
+		log.debug("Buscando todos los empleados en el sistema");
 		return employeeService.findAll();
+	}
+	
+	@RequestMapping(method={RequestMethod.POST})
+	public EmployeeDTO create(@RequestBody EmployeeDTO employee){
+		log.debug(String.format("Vamos a crear el empleado %s", employee));
+		return employeeService.create(employee);
+	}
+	
+	@RequestMapping(value="/{id}", method={RequestMethod.GET})
+	public EmployeeDTO get(@PathVariable("id") Integer id){
+		log.debug(String.format("Recuperando empleado con id: %s",id));
+		return employeeService.getById(id);
+	}
+
+	@RequestMapping(value="/{id}", method={RequestMethod.PUT})
+	public void update(@PathVariable("id") Integer id, @RequestBody EmployeeDTO employee){
+		log.debug(String.format("Vamos a modificar el empleado %s", employee));
+		employeeService.update(employee);
+	}
+	
+	@RequestMapping(value="/{id}", method={RequestMethod.PUT})
+	public void delete(@PathVariable("id") Integer id){
+		log.debug(String.format("Vamos a modificar el empleado con id %s", id));
+		employeeService.delete(id);
 	}
 
 }
