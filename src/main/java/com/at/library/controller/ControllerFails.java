7396b8bff6811f9b864c678c81d8681d768d6bc4
Antonio Miguel Pérez Rivera <antoniom.p.rivera@gmail.com>
2016-08-30 18:22:09 +0200

    Añadida libreria JodaTime en build.gradle

--- src/main/java/com/at/library/controller/ControllerFails.java
+++ src/main/java/com/at/library/controller/ControllerFails.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.at.library.dto.ApiErrorDTO;
+import com.at.library.exception.BookNotFoundException;
 import com.at.library.exception.InvalidDataException;
 
 @ControllerAdvice(basePackages= {"com.at.library.controller"})
@@ -18,5 +19,13 @@ public class ControllerFails {
 	public ApiErrorDTO error(InvalidDataException e){
 		return new ApiErrorDTO(400,e.getMessage());
 	}
+	
+	@ResponseBody
+	@ExceptionHandler(InvalidDataException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ApiErrorDTO error(BookNotFoundException e){
+		return new ApiErrorDTO(404,e.getMessage());
+	}
+	
 	
 }
